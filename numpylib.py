b8540e85f68cdcd95862fce035023abdb7f1dbef
vallabhsangar12 <149781206+vallabhsangar12@users.noreply.github.com>
2024-12-08 17:52:32 +0000

    trigonometric operations

--- numpylib.py
+++ numpylib.py
@@ -227,5 +227,18 @@ x = np.gcd.reduce(arr)
 
 print(x)
 
+#trigonometric operations
+x = np.sin(np.pi/2)
+print(x)
+arr = np.array([np.pi/2, np.pi/3, np.pi/4, np.pi/5])
+x = np.sin(arr)
+print(x)
+#hypo
+base = 3
+perp = 4
+
+x = np.hypot(base, perp)
+
+print(x)
 
 
