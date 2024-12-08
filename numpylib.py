4beb0faeee6b34a7fdc5b952c895a4078b2c0ace
vallabhsangar12 <149781206+vallabhsangar12@users.noreply.github.com>
2024-12-08 17:34:47 +0000

    Arithmatic functions

--- numpylib.py
+++ numpylib.py
@@ -164,3 +164,11 @@ print(z)
 #add functions
 z = np.add(x, y)
 print(z)
+
+#ARITHMATIC FUNCTION
+arr1 = np.array([10, 11, 12, 13, 14, 15])
+arr2 = np.array([20, 21, 22, 23, 24, 25])
+
+newarr = np.add(arr1, arr2)
+
+print(newarr)
