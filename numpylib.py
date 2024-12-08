20c52630bc25002b98c363a2314ced4d749144d1
vallabhsangar12 <149781206+vallabhsangar12@users.noreply.github.com>
2024-12-08 17:37:19 +0000

    arithmetic

--- numpylib.py
+++ numpylib.py
@@ -169,6 +169,14 @@ print(z)
 arr1 = np.array([10, 11, 12, 13, 14, 15])
 arr2 = np.array([20, 21, 22, 23, 24, 25])
 
-newarr = np.add(arr1, arr2)
+ad = np.add(arr1, arr2)
+sub = np.subtract(arr1, arr2)
+mul = np.multiply(arr1, arr2)
+div = np.divide(arr1, arr2)
+print(ad)
+print(sub)
+print(mul)
+print(div)
 
-print(newarr)
+
+
