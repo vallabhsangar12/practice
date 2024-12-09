2a6b4be8ee303f0dff835ad1e3b67306b35dfaad
vallabhsangar12 <149781206+vallabhsangar12@users.noreply.github.com>
2024-12-09 16:52:58 +0000

    statsmodels implementation

--- StockMarketAnalysisUsingArimaSarimaModels.py
+++ StockMarketAnalysisUsingArimaSarimaModels.py
@@ -64,3 +64,19 @@ def stationarity(df):
 from statsmodels.tsa.seasonal   import  seasonal_decompose
 decompose =  seasonal_decompose(df['Close'], model='additive', period=30)
 decompose.plot()
+
+
+from statsmodels.graphics.tsaplots import  plot_acf, plot_pacf
+import matplotlib
+#Original Series
+fig, axes = plt.subplots(3,2, sharex=True)
+axes[0,0].plot(df['Close']); axes[0,0].set_title('Original Series')
+plot_acf (df['Close'], ax=axes[0,1])
+
+#1 st order differencing 
+axes [1,0].plot(df['Close'].diff().diff()); axes[1,0].set_title('1 st order differencing ')
+plot_acf (df['Close'].diff().dropna(), ax=axes[1,1])
+
+ #2nd order differencing 
+axes [2,0].plot(df['Close'].diff().diff()); axes[2,0].set_title('1 st order differencing ')
+plot_acf (df['Close'].diff().dropna(), ax=axes[2,1])
