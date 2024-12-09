38cc8c4b6bc97bf7d19a00580d4fd97b985e4479
vallabhsangar412@gmail.com <149781206+vallabhsangar12@users.noreply.github.com>
2024-12-09 11:15:42 +0000

    define dated to fetch data

--- StockMarketAnalysisUsingArimaSarimaModels.py
+++ StockMarketAnalysisUsingArimaSarimaModels.py
@@ -13,6 +13,18 @@ from statsmodels.tsa.seasonal import seasonal_decompose
 from statsmodels.tsa.statespace.sarimax import SARIMAX
 
 
+#define dated to fetch data
+today = date.today()
+print("TODAYS DATE:",today)
+d1 =today.strftime("%Y-%m-%d")
+end_date = d1
+d2 = date.today()-timedelta(days=365)
+d2 =d2.strftime("%Y-%m-%d")
+start_date = d2
+print("START DATE:",start_date)
+print("END DATE:",end_date)
+
+
 
 
 
